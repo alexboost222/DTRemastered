a225bfcc13e65651d8fbfa0f7c0de327eeafd242
alexboost222 <alexboost222@gmail.com>
2020-10-27 13:10:42 +0300

    add: WIP on tile placer

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UnityEngine;
 
+[ExecuteAlways]
 public class Grid : MonoBehaviour
 {
     [Header("Behaviour")]
@@ -18,25 +19,67 @@ public class Grid : MonoBehaviour
     [Header("Debug")]
     [SerializeField] private Color gridGizmosColor;
 
+    private Plane _plane = new Plane(Vector3.forward, Vector3.zero);
+
+    public float XCellSize => xCellSize;
+    public float YCellSize => yCellSize;
+
+    public bool Raycast(Ray ray, out Vector3 closestCellCenter)
+    {
+        closestCellCenter = Vector3.zero;
+        
+        if (!_plane.Raycast(ray, out float enter))
+            return false;
+
+        closestCellCenter = ClosestCellCenter(ray.GetPoint(enter));
+        return true;
+    }
+    
+    public Vector3 ClosestCellCenter(Vector3 point)
+    {
+        Vector3 projection = _plane.ClosestPointOnPlane(transform.TransformPoint(point));
+
+        Vector3 xLinesOffset = Vector3.up * yCellSize;
+        Vector3 yLinesOffset = Vector3.right * xCellSize;
+        
+        Vector3 xLinesTo =  yLinesOffset * xCellsCount;
+        Vector3 yLinesTo =  xLinesOffset * yCellsCount;
+        
+        Vector3 xPivotOffset = xLinesTo * -xPivot;
+        Vector3 yPivotOffset = yLinesTo * -yPivot;
+        Vector3 pivotOffset = xPivotOffset + yPivotOffset;
+
+        int xLeftCell = (int) (pivotOffset.x / xCellSize);
+        int xRightCell = (int) (xLinesTo.x + pivotOffset.x / xCellSize);
+        
+        int yLeftCell = (int) (pivotOffset.y / yCellSize);
+        int yRightCell = (int) (yLinesTo.y + pivotOffset.y / yCellSize);
+
+        int xCell = (int) Mathf.Clamp(projection.x / xCellSize, xLeftCell, xRightCell);
+        int yCell = (int) Mathf.Clamp(projection.y / yCellSize, yLeftCell, yRightCell);
+
+        return transform.InverseTransformPoint(new Vector3(xCell * xCellSize, yCell * yCellSize, 0));
+    }
+
     private void Draw(Action<Vector3, Vector3> drawLine)
     {
-        int horLinesCount = yCellsCount + 1;
-        int vertLinesCount = xCellsCount + 1;
+        int xLinesCount = yCellsCount + 1;
+        int yLinesCount = xCellsCount + 1;
         
-        Vector3 horLinesOffset = Vector3.up * yCellSize;
-        Vector3 vertLinesOffset = Vector3.right * xCellSize;
+        Vector3 xLinesOffset = Vector3.up * yCellSize;
+        Vector3 yLinesOffset = Vector3.right * xCellSize;
         
-        Vector3 horLinesTo =  vertLinesOffset * xCellsCount;
-        Vector3 vertLinesTo =  horLinesOffset * yCellsCount;
+        Vector3 xLinesTo =  yLinesOffset * xCellsCount;
+        Vector3 yLinesTo =  xLinesOffset * yCellsCount;
         
-        Vector3 xPivotOffset = horLinesTo * -xPivot;
-        Vector3 yPivotOffset = vertLinesTo * -yPivot;
+        Vector3 xPivotOffset = xLinesTo * -xPivot;
+        Vector3 yPivotOffset = yLinesTo * -yPivot;
         Vector3 pivotOffset = xPivotOffset + yPivotOffset;
         
-        DrawParallelLines(horLinesCount, pivotOffset, horLinesTo + pivotOffset,
-            horLinesOffset, drawLine);
-        DrawParallelLines(vertLinesCount, pivotOffset, vertLinesTo + pivotOffset,
-            vertLinesOffset, drawLine);
+        DrawParallelLines(xLinesCount, pivotOffset, xLinesTo + pivotOffset,
+            xLinesOffset, drawLine);
+        DrawParallelLines(yLinesCount, pivotOffset, yLinesTo + pivotOffset,
+            yLinesOffset, drawLine);
     }
 
     private static void DrawParallelLines(int count, Vector3 from, Vector3 to, Vector3 offset, Action<Vector3, Vector3> draw)
---
+++ Assets/Scripts/TilePlacer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class TilePlacer : MonoBehaviour
+{
+    [SerializeField] private Grid grid;
+    [SerializeField] private Camera gameModeCamera;
+    [SerializeField] private Color gizmosColor;
+
+    private Vector3? _closestGridCenter;
+
+    private void Update()
+    {
+        if(grid == null)
+            return;
+
+        Camera raycastCamera = Application.isPlaying ? gameModeCamera : Camera.current;
+        
+        if(raycastCamera == null)
+            return;
+        
+        Ray cameraRay = raycastCamera.ScreenPointToRay(Input.mousePosition);
+        if (grid.Raycast(cameraRay, out Vector3 closestGridCenter))
+            _closestGridCenter = closestGridCenter;
+        else
+            _closestGridCenter = null;
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (_closestGridCenter == null)
+            return;
+        
+        Gizmos.color = gizmosColor;
+        Gizmos.matrix = grid.transform.localToWorldMatrix;
+        Vector3 center = grid.transform.TransformPoint(_closestGridCenter.Value);
+        
+        Vector3 from = center - new Vector3(grid.XCellSize / 2, grid.YCellSize / 2);
+        Vector3 to = center + new Vector3(grid.XCellSize / 2, grid.YCellSize / 2);
+        Gizmos.DrawLine(from, to);
+        
+        from = center - new Vector3(grid.XCellSize / 2, -grid.YCellSize / 2);
+        to = center + new Vector3(grid.XCellSize / 2, -grid.YCellSize / 2);
+        Gizmos.DrawLine(from, to);
+    }
+#endif
+}
